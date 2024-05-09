e7f79734b6b0dbc2b3128b3893ff571f65f092e4
Andrey Ulanov <andy.ulanov@gmail.com>
2024-05-09 20:26:16 +0400

    Initial Commit

---
+++ ImageEditor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageEditor
+//
+//  Created by Andrey Ulanov on 09.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageEditor/ImageEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageEditorApp.swift
+//  ImageEditor
+//
+//  Created by Andrey Ulanov on 09.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
